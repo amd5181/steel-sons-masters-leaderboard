e69bf4052ec754be8fd2e2cf7401da9cb84316f4
amd5181 <adavidfr2006@gmail.com>
2025-03-28 09:37:56 -0400

    Update SteelSonsLeaderboard.jsx

--- SteelSonsLeaderboard.jsx
+++ SteelSonsLeaderboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react"; 
 import Papa from "papaparse";
 
 export default function SteelSonsLeaderboard() {
