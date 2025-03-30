d8efe4ed0d1cd4907fb0b73ce6adaba21b5f2af6
amd5181 <adavidfr2006@gmail.com>
2025-03-30 16:17:55 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./index.html', './*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': { transform: 'translateY(-40px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 0.6s ease-out',
+      },
+    },
   },
   plugins: [],
 };
