08c78f623af3241464a50e200a1fb44688eeb6c7
amd5181 <adavidfr2006@gmail.com>
2025-04-05 13:50:06 -0400

    Update SteelSonsLeaderboard.jsx

--- SteelSonsLeaderboard.jsx
+++ SteelSonsLeaderboard.jsx
@@ -96,7 +96,7 @@ export default function SteelSonsLeaderboard() {
 
         <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-6 mb-4">
           <a
-            href="https://script.google.com/macros/s/AKfycbyVIOzg3NGkO2XkuVCfJBMFsg5BSoeWd5h4W9UE9NmL7ANenO2PJ80RxO2TJLDcK8_a5w/exec"
+            href="https://script.google.com/macros/s/AKfycbziEhRPs7fWs7lhWzva1pkl2C00KCW3qXmSb6zqD14ActFd_YPmDKJQ9H3r74dTZUb_fA/exec"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-yellow-700 hover:bg-yellow-800 text-white font-semibold px-4 py-2 rounded-full shadow transition"
